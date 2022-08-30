b13e1f2568f287621cc1a6919406869e121cc619
Shelby Lee <slee337@uw.edu>
2022-08-30 11:18:09 -0600

    commit tree ec272eecea02eb0c7107f450c2a5559ba2f47e0e

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const eachLink = links.map((link) => (
+    <a key={link} href={'#' + link}>{link}</a>
+  ))
+
+  return <nav>{eachLink}</nav>
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,27 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+
+  //   console.log(projects)
+
+  // const renderProjsPrim = projects.map((project) => (
+  //   <card key={project.id}>{ProjectItem(project)}</card>
+  // ))
+
+
+  const renderProjs = projects.map((project) => (
+    <ProjectItem
+      key={project.id}
+      name={project.name}
+      about={project.about}
+      technologies={project.technologies}
+    />
+  ))
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{renderProjs}</div>
     </div>
   );
 }
